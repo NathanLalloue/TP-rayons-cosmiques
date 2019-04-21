7c6310064e3a70eac4733e6fd2a0e431f9a23fa6
NathanLalloue <49793881+NathanLalloue@users.noreply.github.com>
2019-04-21 19:06:14 +0200

    Update tp.py

--- tp.py
+++ tp.py
@@ -1,57 +1,55 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-f=open(r"C:\Users\natha\Downloads\tp2019-4.txt","r")
-lines=f.readlines()
-data=[]
-for x in lines:
-    data.append(x.split("\t"))
-f.close()
-
-time=[]
-column_1=[]
-column_2=[]
-column_3=[]
-column_4=[]
-
-for i in range(4000, 8002): 
-    #print(i)
-    time.append(float(data[i%4001][0]))
-    column_1.append(float(data[i][1]))
-    column_2.append(float(data[i][2]))
-    column_3.append(float(data[i][3]))
-    column_4.append(float(data[i][4]))
+# -*- coding: utf-8 -*-
+from pylab import*
+from numpy import polynomial as P
+#############Lecture des fichiers#############
+liste_fichiers = [r"C:\Users\natha\Downloads\tp2019pb.txt"]
+liste_size = [2001]
+tt_masses = []
+tab_dE1=[]
+beta_gamma=[]
+for rr in range(0, len(liste_fichiers)):
+     nom_fichier = liste_fichiers[rr]
+     nb_event = liste_size[rr]
+     fd=open(nom_fichier, 'r')
+     all_lignes = fd.readlines()
+     c = 2.99792458*10**8
+     nb = 0
+     for l in range(0, int(len(all_lignes)/nb_event)):
+     #initialisation
+         lignes = all_lignes[l*nb_event:l*nb_event+nb_event]
+         temps = []
+         col1 = []
+         col2 = []
+         col3 = []
+         for i in range(0,nb_event):
+             temps.append(i*2.*10.**-10)
+             tab = lignes[i].split('\t')
+             col1.append(float(tab[1]))
+             col2.append(float(tab[2]))
+             col3.append(float(tab[3]))
     
-plt.figure()
-plt.plot(time, column_1)
-plt.show()
-plt.figure()
-plt.plot(time, column_2)
-plt.show()
-plt.figure()
-plt.plot(time, column_3)
-plt.show()
-plt.figure()
-plt.plot(time, column_4)
-plt.show()
-
-f,ax=plt.subplots(2,2,figsize=(10,3.5))
-ax[0,0].plot(time,column_1,'r.',markersize=1.)
-ax[0,0].set_title('column_1')
-ax[0,0].set_xlabel('temps')
-ax[0,0].set_ylabel('Volt')
-
-ax[0,1].plot(time,column_2,'r.',markersize=1.)
-ax[0,1].set_title('column_2')
-ax[0,1].set_xlabel('temps')
-ax[0,1].set_ylabel('Volt')
-
-ax[1,0].plot(time,column_3,'r.',markersize=1.)
-ax[1,0].set_title('column_3')
-ax[1,0].set_xlabel('temps')
-ax[1,0].set_ylabel('Volt')
-
-ax[1,1].plot(time,column_4,'r.',markersize=1.)
-ax[1,1].set_title('column_4')
-ax[1,1].set_xlabel('temps')
-ax[1,1].set_ylabel('Volt')
+         tab_mini=[]
+         tab_largeur = []
+         tt=[]
+         for col in [col1, col2, col3]:
+                 indice_mini=0
+                 for j in range(1,len(col)):
+                     if col[j]<col[indice_mini]:
+                         indice_mini=j
+                 moyenne = 0.
+                
+                 taille = len(col)
+                 for j in range(0, taille):
+                     moyenne=moyenne+col[j]
+                 moyenne = moyenne/taille
+                 v = min(indice_mini, abs(indice_mini-len(col)-1))-1
+                 
+                 t_m = 0.
+                 v_m = 0.
+                
+                 for k in range(indice_mini-v, indice_mini+v):
+                     t_m += (temps[k])*(col[k]-moyenne)
+                     v_m += (col[k]-moyenne)
+                 tt.append(t_m/v_m) 
+print(tt)
+        
