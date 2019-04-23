162bf3a873ef23e06736669c36840a347cd4bcd5
NathanLalloue <49793881+NathanLalloue@users.noreply.github.com>
2019-04-23 20:39:03 +0200

    Update tp.py

--- tp.py
+++ tp.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from pylab import*
 from numpy import polynomial as P
 #############Lecture des fichiers#############
@@ -51,5 +50,65 @@ for rr in range(0, len(liste_fichiers)):
                      t_m += (temps[k])*(col[k]-moyenne)
                      v_m += (col[k]-moyenne)
                  tt.append(t_m/v_m) 
-print(tt)
+         beta1 = (66.5*10**(-2))/(1.*c*(tt[1]-tt[0]))
+         beta2 = (70.5*10**(-2))/(1.*c*(tt[2]-tt[1]))
+         if beta2<beta1 :
+             gamma1 = 1./sqrt((1.-beta1**2.))
+             gamma2 = 1./sqrt((1.-beta2**2.))
+             mec2 = 0.511 #Mev
+             Pi = 3.14159265359
+             dx = 1.5#cm
+             Z = 82.
+             A = 207.2 #g/mol
+             z = -1.
+             M= 105.66
+             I = 823.*10**(-6)
+             K= 0.307075
+             T_max=(2.*mec2*(beta1**2)*(gamma1**2))/(1.+(2*gamma1*mec2/M)+(mec2/M)**2)
+             P1=(K*(z**2)*Z)/(A*(beta1**2))
+                        
+             dE1 = P1*(0.5*log(abs((2*mec2*beta1**2*gamma1*T_max)/I**2))-beta1**2)
+             print(dE1)
+             beta_gamma.append(beta1*gamma1)
+                         
+                        
+                         ###########Calcul des masses###########
+             mc2 = (dE1)/(gamma1-gamma2)
+             print(l, " Masse : ", mc2)
+             tt_masses.append(mc2)
+                         ############Calcul des erreurs############
+             delta_x1=2.
+             delta_x2=2.
+             delta_t1=1.*10**-10
+             delta_t2=1.*10**-10
+             delta_dx = 1
+             d1 = 66.5*10**-1
+             d2 = 73.*10**-1
+             delta_beta1=(beta1*((delta_x1/d1)+(delta_t1/(tt[1]-tt[0]))))
+             delta_beta2=(beta2*((delta_x2/d2)+(delta_t2/(tt[2]-tt[1]))))
+            
+             delta_gamma1=(gamma1**3*beta1*delta_beta1)
+            
+             delta_gamma2=(gamma2**3*beta2*delta_beta2)
+            
+             f1=2.*delta_beta1/beta1
+             f2=2.*delta_gamma1/gamma1
+             f3=2.*mec2*delta_gamma1/M
+             f4=1.+2.*gamma1*(mec2/M)+(mec2/M)**2
+             delta_T_max = (T_max*(f1+f2-(f3/f4)))
+            
+             f6=K*z**2*Z/(A*beta1**3)
+             f7=(log(2.*mec2*beta1**2*gamma1**2*T_max/I**2)+1.)*delta_beta1
+             f8=K*z**2*Z*delta_gamma1/(A*beta1**2*gamma1)
+             f9=K*z**2*Z*delta_T_max/(2.*A*beta1**2*T_max)
+            
+             delta_dE1 = f6*f7+f8+f9
+            
+             print("beta1=",beta1,"beta2=",beta2,"gamma1=",gamma1,"gamma2=",gamma2,"dE1=",dE1,"T_max=",T_max,"masse=",mc2)
+            
+            
+             delta_m = mc2 *(delta_dE1/dE1 + 10**-1*delta_dx/dx+(delta_gamma1+delta_gamma2)/(gamma1-gamma2))
+            
+             print("delta_beta1=",delta_beta1,"delta_beta2=",delta_beta2,"delta_gamma1=",delta_gamma1,"delta_gamma2=",delta_gamma2,"delta_dE1=",delta_dE1,"delta_T_max=",delta_T_max,"delta_m=",delta_m)
+             nb = nb+ 1
         
